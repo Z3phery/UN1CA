3684dec94d959a907563838102fe91bc69765434
Z3phery <dz.ignacioaguilar@gmail.com>
2026-09-16 23:34:07 -0300

    Nuked touchwizhome hardcode patch
    Signed-off-by: Z3phery <dz.ignacioaguilar@gmail.com>

--- unica/mods/touchwizhome/customize.sh
+++
@@ -1,6 +0,0 @@
-# Disable TouchWizHome updates
-DECODE_APK "system" "system/priv-app/TouchWizHome_2017/TouchWizHome_2017.apk"
-LOG "- Patching Version Code in TouchWizHome_2017.apk"
-EVAL "sed -i \"s/1701105117/999999999/g\" \"$APKTOOL_DIR/system/priv-app/TouchWizHome_2017/TouchWizHome_2017.apk/apktool.yml\""
-# ]
-LOG_STEP_OUT
