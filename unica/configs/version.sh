9b2b61a4cabdc419044c031d149e942fe6d60f64
Z3phery <dz.ignacioaguilar@gmail.com>
2026-09-16 23:49:29 -0300

    unica: configs: add unofficial tag
    Signed-off-by: Z3phery <dz.ignacioaguilar@gmail.com>

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -13,3 +13,6 @@ ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 
 # Append current commit hash to version name
 ROM_VERSION+="-$(git rev-parse --short HEAD 2> /dev/null || echo "null")"
+
+# Unofficial tag
+ROM_VERSION+="-Unofficial"
